f579c17033b8623df49094c1b5ce5dba6cc05911
Glitch (treasure-gossamer-wok) <none>
2021-12-31 08:04:19 +0000

    🏄🎁 Checkpoint ./komutlar/bot-istatistik.js:40373472/40 ./komutlar/bot-komutlar.js:40373472/193 ./komutlar/bot-prefix.js:40373472/387 ./komutlar/bot-öneri.js:40373472/317

--- komutlar/bot-istatistik.js
+++ komutlar/bot-istatistik.js
@@ -7,7 +7,7 @@ const ayarlar = require('../ayarlar/bot.json');
 require('moment-duration-format');
 exports.run = async(client, message, args) => {
 
-let lordcreative = new Discord.MessageEmbed()
+let darkwill = new Discord.MessageEmbed()
 .setThumbnail(`https://cdn.discordapp.com/attachments/749380170351116290/750088540288712914/B0oBpM.png`)
 .addField("__**Bot Verileri**__", `<a:developer:837990618667548682> **Toplam Sunucu** **|**  **${client.guilds.cache.size}** \n <a:developer:837990618667548682>  **Toplam Kullanıcı** **|** **${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}** \n <a:developer:837990618667548682>  **Toplam Kanal** **|** **${client.channels.cache.size}**`)
 .addField("__**Bot Geliştiricisi**__", `<a:developer:837990618667548682>  **Bot Sahibi**  <@408496730644938752> \n **🖤ʙʟᴀᴄᴋ ғʟᴏᴡᴇʀ🥀#6887** \n\n <a:developer:837990618667548682> \ **Bot Geliştiricisi**  <@408496730644938752> \n **🖤ʙʟᴀᴄᴋ ғʟᴏᴡᴇʀ🥀#6887** \n\n <a:developer:837990618667548682>  **Bot Sahibi**  <a:developer:837990618667548682> \n **🖤ʙʟᴀᴄᴋ ғʟᴏᴡᴇʀ🥀#6887** \n`)
@@ -16,7 +16,7 @@ let lordcreative = new Discord.MessageEmbed()
 .addField("**__Müzik Oynatılan__** ", "<a:developer:837990618667548682>  " +client.voice.connections.size + " Sunucu", true)
     .setImage(`https://cdn.discordapp.com/attachments/918504555271028776/926380656005615656/standard.gif`)
 .setColor("#ffffff")
-message.channel.send(lordcreative)
+message.channel.send(darkwill)
 }
 
 exports.config = {
--- komutlar/bot-komutlar.js
+++ komutlar/bot-komutlar.js
@@ -3,12 +3,12 @@ const Discord = require('discord.js')
 exports.run = async (client, message, args, level) => {
     
     const embed = new Discord.MessageEmbed()
-    .setTitle(`Lord Creative - Komut Sayısı`)
-    .setDescription('**\n Lord Creative Bot | Toplam**  **`' + client.commands.size + '`** **Komut Vardır!**')
+    .setTitle(`Darkwill - Komut Sayısı`)
+    .setDescription('**\n <a:tac:774263713501806623>Darkwill Bot | Toplam**  **`' + client.commands.size + '`** **Komut Vardır!**')
     .setColor("#ffffff")
     .setThumbnail('https://i.ibb.co/s2qGRFx/kod.png')
     .setTimestamp()
-    .setFooter("Lord Creative | Gelişmiş Türkçe Bot | 2020" , client.user.avatarURL())
+    .setFooter("Darkwill | Gelişmiş Türkçe Bot | 2020" , client.user.avatarURL())
 
     return message.channel.send(embed);
     
